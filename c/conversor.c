d00e238e54fab0f777806b4f2402ea1c0aab434a
Mozart Moura de Marias Silva <mozartmoura15@gmail.com>
2026-02-26 08:49:21 -0300

    optimized code

--- c/conversor.c
+++ c/conversor.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-    float real, dolar;
+    float real, dolar, cotacao = 5.3;
     printf("Digite o valor em real: ");
     scanf("%f", &real);
-    dolar = real / 5.3;
+    dolar = real / cotacao;
     printf("O valor em dolar é: %.2f", dolar);
     return 0;
 }
